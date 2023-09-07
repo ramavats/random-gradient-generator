977e960aecd9c081a19ae14683115688e34607e5
Rama Shankar Jha <guy55222@gmail.com>
2023-09-07 15:37:32 +0530

    changed text to icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
 
   return (
-    <div className="navbar navbar-floating bg-gradient-to-r from-rose-700 to-pink-600 lg:rounded-full rounded-none sticky lg:top-5 top-0 drop-shadow-md">
+    <div className="navbar navbar-floating bg-gradient-to-r from-rose-700 to-pink-600 rounded-full sticky lg:top-5 top-0 drop-shadow-md">
 	<div className="navbar-start">
 		<a className="navbar-item font-bold">Gradient Generator</a>
 	</div>
